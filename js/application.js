c95eedf62e6969bc8b25e2596179cb8f6cf0e86a
Rachel <rachella@gmail.comOB>
2015-07-14 00:13:44 +0800

    Added accordion to main game. Added scroll to anchor.

--- js/application.js
+++ js/application.js
@@ -1,5 +1,13 @@
 $(document).ready (function() {
-  
-});
+	$(".accordion div").slideToggle("fast");
+	$(".accordion h3").click(function () {
+	    $(this).next(".pane").slideToggle("slow").siblings(".pane:visible").slideUp("slow");
+	});
 
-	
+	$(".btn-lg").click(function() {
+		$("html, body").animate({
+			scrollTop: $("#Q1").offset().top
+        }, 1000);
+	});	
+
+});
