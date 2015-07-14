e13eda58f40d07a14c82029af9e819ba1b180938
Rachel <rachella@gmail.comOB>
2015-07-14 13:54:15 +0800

    Added fuction to add player names to aside

--- js/application.js
+++ js/application.js
@@ -4,11 +4,32 @@ $(document).ready (function() {
 	    $(this).next(".pane").slideToggle("slow").siblings(".pane:visible").slideUp("slow");
 	});
 	$(".startingButton").click(function() {
-		$("html, body").animate({
+    $("html, body").animate({
 			scrollTop: $("#q1").offset().top
         }, 1000);
 	});	
 
+  $(".submit-name").click(function() {
+    var addName = $('#1stplayer').val();
+    $('aside').append('addName');
+  })
+
 });
 
- 
+
+
+// $(function() {
+//      var offset = $("#playerNames").offset();
+//      var topPadding = 10;
+//      // $(window).scroll(function() {
+//      //     $(window).scrollTop() > offset.top) {
+//      //         $("#box").stop().animate({
+//      //             marginTop: $(window).scrollTop() - offset.top + topPadding
+//      //         });
+//      //     });
+//      // });
+//  });
+
+// $("confirm").click(function() {
+// $(.correct).toggleClass("correct-clicked")
+// });
