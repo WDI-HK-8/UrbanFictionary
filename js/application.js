57959fb9c982f48658f60746e87f2d092c682369
Rachel <rachella@gmail.comOB>
2015-07-14 11:15:32 +0800

    Added accordion to main game as well as scrolling feature using anchors and jquery.

--- js/application.js
+++ js/application.js
@@ -1,16 +1,17 @@
-
-
-
 $(document).ready (function() {
 	$(".accordion div").slideToggle("fast");
 	$(".accordion h3").click(function () {
 	    $(this).next(".pane").slideToggle("slow").siblings(".pane:visible").slideUp("slow");
 	});
 
-	$(".btn-lg").click(function() {
+	$(".startingButton").click(function() {
 		$("html, body").animate({
 			scrollTop: $("#Q1").offset().top
         }, 1000);
 	});	
 
+	
+
 });
+
+ 
