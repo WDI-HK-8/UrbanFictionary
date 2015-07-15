1a39caa16a240d2ee5a97a61b8142cae69ec8712
Rachel <rachella@gmail.comOB>
2015-07-15 15:07:16 +0800

    Randomize questions function

--- js/application.js
+++ js/application.js
@@ -1,4 +1,5 @@
-var content = [{'word':'Just Ducky','answer':'correct', 'fullAnswer':'Calm on the surface, paddling like crazy beneath.'},{'word':'Eye Cabbage','answer':'correct', 'fullAnswer':'The complete opposite of eye candy'},{'word':'Moron First Base','answer':'correct', 'fullAnswer':'Engaging in hand to hand'},{'word':'Sausage Fear','answer':'correct', 'fullAnswer':'Straight men who are afraid of showing any physical or emotional bond with'},{'word':'Trust Fail','answer':'correct', 'fullAnswer':'A poorly excuted trust fall'},{'word':'Cinderfella','answer':'correct', 'fullAnswer':'A man who must be home by midnight'},{'word':'Crappy Ending','answer':'correct', 'fullAnswer':'Being so relaxed at the end of a massage that one poops during the massage'},{'word':'Fling Cleaning','answer':'correct', 'fullAnswer':'When one cleans one\'s room solely because they think they will be getting laid that night'},{'word':'Grave Digging','answer':'correct', 'fullAnswer':'Trying to engage in intercourse with a much older person'},{'word':'Falawesome','answer':'correct', 'fullAnswer':'Falafel that is heaven in your mouth'},{'word':'Nicholas Cage Syndrome','answer':'correct', 'fullAnswer':'When you have the same facial expression no matter what emotion you\'re supposed to be showing.'}];
+// original
+var content = [{'word':'Just Ducky','answer':'correct', 'fullAnswer':'Calm on the surface, paddling like crazy beneath'},{'word':'Eye Cabbage','answer':'correct', 'fullAnswer':'The complete opposite of eye candy'},{'word':'Moron First Base','answer':'correct', 'fullAnswer':'Engaging in hand to hand'},{'word':'Sausage Fear','answer':'correct', 'fullAnswer':'Straight men who are afraid of showing any physical or emotional bond with'},{'word':'Trust Fail','answer':'correct', 'fullAnswer':'A poorly excuted trust fall'},{'word':'Cinderfella','answer':'correct', 'fullAnswer':'A man who must be home by midnight'},{'word':'Crappy Ending','answer':'correct', 'fullAnswer':'Being so relaxed at the end of a massage that one poops during the massage'},{'word':'Fling Cleaning','answer':'correct', 'fullAnswer':'When one cleans one\'s room solely because they think they will be getting laid that night'},{'word':'Grave Digging','answer':'correct', 'fullAnswer':'Trying to engage in intercourse with a much older person'},{'word':'Falawesome','answer':'correct', 'fullAnswer':'Falafel that is heaven in your mouth'},{'word':'Nicholas Cage Syndrome','answer':'correct', 'fullAnswer':'When you have the same facial expression no matter what emotion you\'re supposed to be showing'}];
 
 $(document).ready (function() {
 
@@ -30,23 +31,34 @@ $(document).ready (function() {
   var selection = content[Math.floor(Math.random()*11)];
   var type = selection['answer'];
   var fullAnswer = selection['fullAnswer'];
+  // //new
+  var incorrect = selection['incorrect'];
+  var incorrect2 = selection['incorrect2'];
   $('.word').html(selection['word']);
+  $('.fullAnswer').html(fullAnswer);
+  $('.incorrect').html(incorrect);
+  $('.incorrect2').html(incorrect2);  
   $('.result').hide();
-  $('.generate').hide();
+  $('.next-question').hide();
 
-
-  $('.choice').click(function(e) {
-    var pickedAnswer = e.target.class;
+  $('.choice').click(function(){
+    var pickedAnswer = $(this).text();
     $('.result').show();
-    $('.generate') .show();     
-    if (pickedAnswer == type) {
-      $('.result').html('Yes! Gold star to you, you fantastic sexy know-it-all');
+    $('.next-question').show();  
+    if (pickedAnswer == fullAnswer) {
+      $('.result').html('Yes! Gold star to you, you fantastic sexy know-it-all!');
       $('.result').css('color', '#FFDA64');
       } else {
-      $('.result').html('Incorrect. Your answer sucked.');
-      $('.result').css('color', '#9B2500')
-    };
-  });
+      $('.result').html('Incorrect. Your answer sucked. The correct answer is:'+ '<br /><br />' + '"' + fullAnswer + '."');
+      $('.result').css('color', '#F60118')
+    }; 
+  })
+
+  $('.next-question').click(function() {
+  //   $('.accordion div').slideToggle('fast');
+  //   $('.accordion h2 ').click(function () {
+  //     $(this).next('.pane').slideToggle('slow').siblings('.pane:visible').slideUp('slow');
+  })
 
   //Option to restart
   $('.play-again').click(function() {
